6fd45d37dbd2a0fd325da73e7ce34c43c4642d47
Alucard <smpark@ezstorage.co.kr>
2021-03-20 19:36:12 +0900

    add pg

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const { ApolloServer, gql } = require("apollo-server");
+const { Client } = require("pg");
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
 
 const typeDefs = gql`
   type Book {
